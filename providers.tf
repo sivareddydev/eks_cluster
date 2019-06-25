50239ff35227dc6b9f651a60b04ed7cde182fa51
sivareddy7204 <35861579+sivareddy7204@users.noreply.github.com>
2019-06-25 16:40:19 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,13 +3,7 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "techbloc-terraform-data"
-    key    = "openshift-311"
-    region = "eu-west-1"
-  }
-}
+
 
 data "aws_region" "current" {}
 
